b15ac223527332e50cdf6b8886a361cf765cf0ec
parthami <chandratreya.parth@gmail.com>
2017-12-27 19:11:48 +0000

    Added MFA layout

--- app/src/main/java/parth/mfa_fingerprint/MainActivity.kt
+++ app/src/main/java/parth/mfa_fingerprint/MainActivity.kt
@@ -8,5 +8,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val b = R.id.toggleButton
+//        b.setOnClickListener(object : View.OnClickListener() {
+//            fun onClick(v: View) {
+//                // your click actions go here
+//            }
+//        })
     }
 }
