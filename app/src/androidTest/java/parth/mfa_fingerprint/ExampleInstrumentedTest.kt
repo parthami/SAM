4d734a6b7949e3fd51ba89ee584a35ce355fed48
parthami <chandratreya.parth@gmail.com>
2017-12-27 18:02:28 +0000

    Initial commit

---
+++ app/src/androidTest/java/parth/mfa_fingerprint/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package parth.mfa_fingerprint
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("parth.mfa_fingerprint", appContext.packageName)
+    }
+}
