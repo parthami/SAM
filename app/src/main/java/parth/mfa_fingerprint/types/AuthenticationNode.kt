7c347bb5503b2ce46b2fe5fbfe472bf6b6d1d7d2
parthami <chandratreya.parth@gmail.com>
2018-03-20 11:06:43 +0000

    Reverted manual disabling of QR code factor adjust design of some factors added camera icon for qr button

--- app/src/main/java/parth/mfa_fingerprint/types/AuthenticationNode.kt
+++ app/src/main/java/parth/mfa_fingerprint/types/AuthenticationNode.kt
@@ -6,7 +6,7 @@ package parth.mfa_fingerprint.types
 
 enum class AuthenticationNode(val label: String, val affectedBy : Enviroment, var probability: Double, var enabled : Boolean) {
     FINGERPRINT("Fingerprint",Enviroment.NONE, 0.14, true),
-    QR("QR",Enviroment.LIGHT, 0.14, false),
+    QR("QR",Enviroment.LIGHT, 0.14, true),
     PASSWORD("Password",Enviroment.NONE, 0.14, true),
     BLANK("Blank",Enviroment.SOUND, 0.14, true),
     LOCATION("Location", Enviroment.NONE, 0.14, true),
